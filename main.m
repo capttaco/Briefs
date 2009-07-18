644d66a0965d4c624dcd85d9674ed54c1e6af9c2
Rob Rhyne <rob@robrhyne.com>
2009-07-17 22:16:18 -0400

    Updated header to point to LICENSE file re:licensing terms.

--- main.m
+++ main.m
@@ -3,7 +3,7 @@
 //  Briefs
 //
 //  Created by Rob Rhyne on 6/30/09.
-//  Copyright Digital Arch Design, 2009. All rights reserved.
+//  Copyright Digital Arch Design, 2009. See LICENSE file for details.
 //
 
 #import <UIKit/UIKit.h>
