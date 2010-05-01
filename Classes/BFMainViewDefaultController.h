fafbeb09bbd863802edc08af04a7caba5cfcdaf4
Rob Rhyne <rob@avenger.home>
2010-05-01 10:48:43 -0400

    Fuck me, merging can be a bitch. Also, make sure you're on the right branch when switching machines. Can make for difficult issues later.

---
+++ Classes/BFMainViewDefaultController.h
@@ -0,0 +1,15 @@
+//
+//  BFMainViewDefaultController.h
+//  Briefs
+//
+//  Created by Rob Rhyne on 5/1/10.
+//  Copyright 2010 Digital Arch Design. All rights reserved.
+//
+
+
+@interface BFMainViewDefaultController : BFTableViewController 
+{
+    
+}
+
+@end
